b7fdffcb6398699ef2a1cf574f5a634fdab9edb0
bradingcdsfdf-cell <bradingcdsfdf@gmail.com>
2026-06-21 22:59:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,24 @@ app.get("/player/:userId", async (req, res) => {
 	const userId = req.params.userId;
 
 	try {
-		const response = await fetch(`https://www.rolimons.com/playerapi/player/${userId}`);
+		const response = await fetch(`https://www.rolimons.com/playerapi/player/${userId}`, {
+			headers: {
+				"User-Agent": "Mozilla/5.0"
+			}
+		});
+
 		const data = await response.json();
 
 		res.json({
 			value: data.value || 0,
-			rap: data.rap || 0
+			rap: data.rap || 0,
+			debug: data
 		});
 	} catch (err) {
 		res.json({
 			value: 0,
-			rap: 0
+			rap: 0,
+			error: err.message
 		});
 	}
 });
