874f99a868093af9283e49bad4c975fa1351e1d5
bradingcdsfdf-cell <bradingcdsfdf@gmail.com>
2026-06-21 22:47:45 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.get("/player/:userId", async (req, res) => {
+	const userId = req.params.userId;
+
+	try {
+		const response = await fetch(`https://www.rolimons.com/playerapi/player/${userId}`);
+		const data = await response.json();
+
+		res.json({
+			value: data.value || 0,
+			rap: data.rap || 0
+		});
+	} catch (err) {
+		res.json({ value: 0, rap: 0 });
+	}
+});
+
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
